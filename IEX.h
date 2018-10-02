5cf7751be258331358f7fd48be83f2c2311202a7
Michael Ferguson <mpherg@gmail.com>
2018-10-01 21:17:57 -0600

    Implement getCompanyInfo
    This is a fairly major commit to the code. There are a few things that
    were changed:
    
    1. The method `sendGetRequest` was refactored out of the Price class,
    because it is the same for all requests.
    2. clang-formatted all the things.
    3. Added structure and class to get the Company information.
    4. `main` was refactored to set a simplified pattern for adding
    different APIs.
    
    Addresses #1.

--- IEX.h
+++ IEX.h
@@ -1,13 +1,14 @@
 #ifdef __APPLE__
-#include <json/json.h> // My MacOS looks for this path. Maybe a new version when installed via HomeBrew?
+#include <json/json.h>  // My MacOS looks for this path. Maybe a new version when installed via HomeBrew?
 #else
-#include <jsoncpp/json/json.h> // My Linux looks for this path. How about you?
+#include <jsoncpp/json/json.h>  // My Linux looks for this path. How about you?
 #endif
 #include <curl/curl.h>
-#include <locale> // std::locale, std::isdigit
-#include <string>
 #include <iostream>
-#include <memory> // std::unique_ptr
+#include <locale>  // std::locale, std::isdigit
+#include <memory>  // std::unique_ptr
+#include <string>
+#include <vector>
 
 #ifndef IEX_H_INCLUDED
 #define IEX_H_INCLUDED
@@ -15,47 +16,105 @@
 
 namespace IEX {
     namespace Resources {
-        void sendGetRequest(Json::Value &data, std::string url);
+        static void sendGetRequest(Json::Value& jsonData,
+                                   const std::string url,
+                                   std::size_t (*callback)(const char*,
+                                                           std::size_t,
+                                                           std::size_t,
+                                                           std::string*)) {
+            CURL* curl = curl_easy_init();
 
-        struct PriceData {
-          std::string called_endpoint;
-          std::string stock_symbol;
-          double latest_price;
+            curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+            curl_easy_setopt(curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+            curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10);
+            curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+            long int httpCode(0);
+            std::unique_ptr<std::string> httpData(new std::string());
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, callback);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, httpData.get());
+            curl_easy_perform(curl);
+            curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &httpCode);
+            curl_easy_cleanup(curl);
+
+            // Json::CharReaderBuilder jsonReader;
+            // Json::CharReader* reader;
+
+            Json::Reader jsonReader;
+            jsonReader.parse(*httpData, jsonData);
+        }
+
+        struct CompanyData {
+            std::string called_endpoint;
+            std::string stock_symbol;
+            std::string company_name;
+            std::string exchange;
+            std::string industry;
+            std::string website;
+            std::string description;
+            std::string CEO;
+            std::string issue_type;
+            std::string sector;
+            std::vector<std::string> tags;
         };
 
-        class Price {
+        class Company {
         public:
-            static std::size_t callback(
-            const char* in,
-            std::size_t size,
-            std::size_t num,
-            std::string* out)
-            {
+            static std::size_t callback(const char* in,
+                                        std::size_t size,
+                                        std::size_t num,
+                                        std::string* out) {
                 const std::size_t totalBytes(size * num);
                 out->append(in, totalBytes);
                 return totalBytes;
             }
 
-            static void sendGetRequest(Json::Value &jsonData, const std::string url){
-                CURL* curl = curl_easy_init();
+            static CompanyData get(const std::string stock_symbol) {
+                std::string url = IEX_API_V1_ENDPOINT;
+                url += "/stock/" + std::string(stock_symbol) + "/company";
 
-                curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-                curl_easy_setopt(curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
-                curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10);
-                curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-                long int httpCode(0);
-                std::unique_ptr<std::string> httpData(new std::string());
-                curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, callback);
-                curl_easy_setopt(curl, CURLOPT_WRITEDATA, httpData.get());
-                curl_easy_perform(curl);
-                curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &httpCode);
-                curl_easy_cleanup(curl);
+                Json::Value jsonData;
+                sendGetRequest(jsonData, url, callback);
 
-                // Json::CharReaderBuilder jsonReader;
-                // Json::CharReader* reader;
+                CompanyData cd;
 
-                Json::Reader jsonReader;
-                jsonReader.parse(*httpData, jsonData);
+                // clang-format off
+                cd.called_endpoint = url;
+                cd.stock_symbol    = stock_symbol;
+                cd.company_name    = jsonData["companyName"].asString();
+                cd.exchange        = jsonData["exchange"].asString();
+                cd.industry        = jsonData["industry"].asString();
+                cd.website         = jsonData["website"].asString();
+                cd.description     = jsonData["description"].asString();
+                cd.CEO             = jsonData["CEO"].asString();
+                cd.issue_type      = jsonData["issue_type"].asString();
+                cd.sector          = jsonData["sector"].asString();
+                // clang-format on
+
+                // Iterate over all tags and add to vector
+                auto tags = jsonData["tags"];
+                for (const auto& t : tags) {
+                    cd.tags.push_back(t.asString());
+                }
+
+                return cd;
+            }
+        };
+
+        struct PriceData {
+            std::string called_endpoint;
+            std::string stock_symbol;
+            double latest_price;
+        };
+
+        class Price {
+        public:
+            static std::size_t callback(const char* in,
+                                        std::size_t size,
+                                        std::size_t num,
+                                        std::string* out) {
+                const std::size_t totalBytes(size * num);
+                out->append(in, totalBytes);
+                return totalBytes;
             }
 
             static PriceData get(const std::string stock_symbol) {
@@ -63,7 +122,7 @@ namespace IEX {
                 url += "/stock/" + std::string(stock_symbol) + "/price";
 
                 Json::Value jsonData;
-                sendGetRequest(jsonData, url);
+                sendGetRequest(jsonData, url, callback);
 
                 PriceData pd;
                 pd.called_endpoint = url;
@@ -73,7 +132,7 @@ namespace IEX {
                 return pd;
             }
         };
-    }
-}
+    }  // namespace Resources
+}  // namespace IEX
 
-#endif // IEX_H_INCLUDED
+#endif  // IEX_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -2,14 +2,41 @@
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
+void print_price(const std::string& symbol) {
     IEX::Resources::PriceData stock;
-    stock = IEX::Resources::Price::get(string(argv[1]));
+    stock = IEX::Resources::Price::get(symbol);
 
     cout << "Called Endpoint: " << stock.called_endpoint << endl;
     cout << "Stock Symbol: " << stock.stock_symbol << endl;
     cout << "Latest Price: $" << stock.latest_price << endl;
+}
 
-    return 0;
+void print_company(const std::string& symbol) {
+    IEX::Resources::CompanyData cd = IEX::Resources::Company::get(symbol);
+    cout << "Called Endpoint: " << cd.called_endpoint << endl;
+    cout << "Company Name: " << cd.company_name << endl;
+    cout << "Exchange: " << cd.exchange << endl;
+    cout << "Industry: " << cd.industry << endl;
+    cout << "Website: " << cd.website << endl;
+    cout << "Description: " << cd.description << endl;
+    cout << "CEO: " << cd.CEO << endl;
+    cout << "Issue type: " << cd.issue_type << endl;
+    cout << "Sector: " << cd.sector << endl;
+    cout << "Tags:" << endl;
+    for (const auto& t : cd.tags) {
+        cout << "  " << t << endl;
+    }
+}
+
+int main(int argc, char* argv[]) {
+    if (argc == 2) {
+        std::string symbol = argv[1];
+        print_company(symbol);
+        print_price(symbol);
+        return 0;
+    } else {
+        cout << "Error! Usage: " << argv[0] << " [stock symbol]" << endl;
+        return 1;
+    }
 }
 
