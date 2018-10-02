d15e1a45f88739081f9f55b025552cc3b82e4b45
Michael Ferguson <mpherg@gmail.com>
2018-10-02 11:52:46 -0600

    Implement getKeyStats
    Note that there are some values that return null, which cannot be
    represented in C++ primitive types such as int and double. In this case
    we arbitrarily choose zero, which admittedly is not the same as null and
    may in fact be a valid (or invalid) numeric value in context.
    
    Addresses #3.

--- IEX.h
+++ IEX.h
@@ -4,6 +4,7 @@
 #include <jsoncpp/json/json.h>  // My Linux looks for this path. How about you?
 #endif
 #include <curl/curl.h>
+#include <cstdint>  // uint64_t, etc.
 #include <iostream>
 #include <locale>  // std::locale, std::isdigit
 #include <memory>  // std::unique_ptr
@@ -16,6 +17,38 @@
 
 namespace IEX {
     namespace Resources {
+        static std::string getString(const Json::Value& v) {
+            if (!v) {
+                return "";
+            } else {
+                return v.asString();
+            }
+        }
+
+        static uint64_t getUInt64(const Json::Value& v) {
+            if (!v) {
+                return 0;
+            } else {
+                return v.asUInt64();
+            }
+        }
+
+        static int getInt(const Json::Value& v) {
+            if (!v) {
+                return 0;
+            } else {
+                return v.asInt();
+            }
+        }
+
+        static double getDouble(const Json::Value& v) {
+            if (!v) {
+                return 0;
+            } else {
+                return v.asDouble();
+            }
+        }
+
         static void sendGetRequest(Json::Value& jsonData,
                                    const std::string url,
                                    std::size_t (*callback)(const char*,
@@ -42,6 +75,136 @@ namespace IEX {
             Json::Reader jsonReader;
             jsonReader.parse(*httpData, jsonData);
         }
+
+        struct KeyStatsData {
+            std::string called_endpoint;
+            std::string stock_symbol;
+            std::string company_name;
+            uint64_t marketcap;
+            double beta;
+            double week52high;
+            double week52low;
+            double week52change;
+            double shortInterest;
+            std::string shortDate;
+            double dividendRate;
+            double dividendYield;
+            std::string exDividendDate;
+            double latestEPS;
+            std::string latestEPSDate;
+            uint64_t sharesOutstanding;
+            uint64_t _float;
+            double returnOnEquity;
+            double consensusEPS;
+            int numberOfEstimates;
+            uint64_t EBITDA;
+            uint64_t revenue;
+            uint64_t grossProfit;
+            uint64_t cash;
+            uint64_t debt;
+            double ttmEPS;
+            double revenuePerShare;
+            double revenuePerEmployee;
+            double peRatioHigh;
+            double peRatioLow;
+            double EPSSurpriseDollar;
+            double EPSSurprisePercent;
+            double returnOnAssets;
+            double returnOnCapital;
+            double profitMargin;
+            double priceToSales;
+            double priceToBook;
+            double day200MovingAvg;
+            double day50MovingAvg;
+            double institutionPercent;
+            double insiderPercent;
+            double shortRatio;
+            double year5ChangePercent;
+            double year2ChangePercent;
+            double year1ChangePercent;
+            double ytdChangePercent;
+            double month6ChangePercent;
+            double month3ChangePercent;
+            double month1ChangePercent;
+            double day5ChangePercent;
+        };
+
+        class KeyStats {
+        public:
+            static std::size_t callback(const char* in,
+                                        std::size_t size,
+                                        std::size_t num,
+                                        std::string* out) {
+                const std::size_t totalBytes(size * num);
+                out->append(in, totalBytes);
+                return totalBytes;
+            }
+
+            static KeyStatsData get(const std::string stock_symbol) {
+                std::string url = IEX_API_V1_ENDPOINT;
+                url += "/stock/" + std::string(stock_symbol) + "/stats";
+
+                Json::Value jsonData;
+                sendGetRequest(jsonData, url, callback);
+
+                KeyStatsData ksd;
+
+                // clang-format off
+                ksd.called_endpoint     = url;
+                ksd.stock_symbol        = stock_symbol;
+                ksd.company_name        = getString(jsonData["companyName"]);
+                ksd.marketcap           = getUInt64(jsonData["marketcap"]);
+                ksd.beta                = getDouble(jsonData["beta"]);
+                ksd.week52high          = getDouble(jsonData["week52high"]);
+                ksd.week52low           = getDouble(jsonData["week52low"]);
+                ksd.week52change        = getDouble(jsonData["week52change"]);
+                ksd.shortInterest       = getDouble(jsonData["shortInterest"]);
+                ksd.shortDate           = getString(jsonData["shortDate"]);
+                ksd.dividendRate        = getDouble(jsonData["dividendRate"]);
+                ksd.dividendYield       = getDouble(jsonData["dividendYield"]);
+                ksd.exDividendDate      = getString(jsonData["exDividentDate"]);
+                ksd.latestEPS           = getDouble(jsonData["latestEPS"]);
+                ksd.latestEPSDate       = getString(jsonData["latestEPSDate"]);
+                ksd.sharesOutstanding   = getUInt64(jsonData["sharedOutstanding"]);
+                ksd._float              = getUInt64(jsonData["float"]);
+                ksd.returnOnEquity      = getDouble(jsonData["returnOnEquity"]);
+                ksd.consensusEPS        = getDouble(jsonData["consensusEPS"]);
+                ksd.numberOfEstimates   = getInt(jsonData["numberOfEstimates"]);
+                ksd.EBITDA              = getUInt64(jsonData["EBITDA"]);
+                ksd.revenue             = getUInt64(jsonData["revenue"]);
+                ksd.grossProfit         = getUInt64(jsonData["grossProfit"]);
+                ksd.cash                = getUInt64(jsonData["cash"]);
+                ksd.debt                = getUInt64(jsonData["debut"]);
+                ksd.ttmEPS              = getDouble(jsonData["ttmEPS"]);
+                ksd.revenuePerShare     = getDouble(jsonData["revenuePerShare"]);
+                ksd.revenuePerEmployee  = getDouble(jsonData["revenuePerEmployee"]);
+                ksd.peRatioHigh         = getDouble(jsonData["peRatioHigh"]);
+                ksd.peRatioLow          = getDouble(jsonData["peRatioLow"]);
+                ksd.EPSSurpriseDollar   = getDouble(jsonData["EPSSurpriseDollar"]);
+                ksd.EPSSurprisePercent  = getDouble(jsonData["EPSSurprisePercent"]);
+                ksd.returnOnAssets      = getDouble(jsonData["returnOnAssets"]);
+                ksd.returnOnCapital     = getDouble(jsonData["returnOnCapital"]);
+                ksd.profitMargin        = getDouble(jsonData["profitMargin"]);
+                ksd.priceToSales        = getDouble(jsonData["priceToSales"]);
+                ksd.priceToBook         = getDouble(jsonData["priceToBook"]);
+                ksd.day200MovingAvg     = getDouble(jsonData["day200MovingAvg"]);
+                ksd.day50MovingAvg      = getDouble(jsonData["day50MovingAvg"]);
+                ksd.institutionPercent  = getDouble(jsonData["institutionPercent"]);
+                ksd.insiderPercent      = getDouble(jsonData["insiderPercent"]);
+                ksd.shortRatio          = getDouble(jsonData["shortRatio"]);
+                ksd.year5ChangePercent  = getDouble(jsonData["year5ChangePercent"]);
+                ksd.year2ChangePercent  = getDouble(jsonData["year2ChangePercent"]);
+                ksd.year1ChangePercent  = getDouble(jsonData["year1ChangePercent"]);
+                ksd.ytdChangePercent    = getDouble(jsonData["ytdChangePercent"]);
+                ksd.month6ChangePercent = getDouble(jsonData["month6ChangePercent"]);
+                ksd.month3ChangePercent = getDouble(jsonData["month3ChangePercent"]);
+                ksd.month1ChangePercent = getDouble(jsonData["month1ChangePercent"]);
+                ksd.day5ChangePercent   = getDouble(jsonData["day5ChangePercent"]);
+                // clang-format on
+
+                return ksd;
+            }
+        };
 
         struct CompanyData {
             std::string called_endpoint;
